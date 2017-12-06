688b527f261e80ff3ce8df2368cb0546db2a718c
zhangpengfei12 <229269553@qq.com>
2017-12-06 17:13:47 +0800

    修改图标及toolbar

--- app/src/main/java/com/example/tk/music_mvc/mymusic/controller/MusicFragment.java
+++ app/src/main/java/com/example/tk/music_mvc/mymusic/controller/MusicFragment.java
@@ -7,7 +7,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
 
 import com.example.tk.music_mvc.R;
 import com.example.tk.music_mvc.mymusic.model.MusicModel;
@@ -19,27 +22,75 @@ import java.util.List;
  * Created by User on 2017/11/27.
  */
 
-public class MusicFragment extends Fragment implements View.OnClickListener{
-    private Button btn_previous;
-    private Button btn_play;
-    private Button btn_next;
+public class MusicFragment extends Fragment {
+    private ImageView btn_previous;
+    private ImageView btn_play;
+    private ImageView btn_next;
+    private ImageView music_img;
     private List<MusicModel> list;
     private MusicModel musicModel;
     private MediaPlayer myMediaPlayer;
+    private Animation animation;
     static int currentListItem;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.music_player_layout, null);
+        View view = inflater.inflate(R.layout.music_player_layout, container, false);
 
         list = MusicUtil.getMusicData(getActivity());
 
         myMediaPlayer=new MediaPlayer();
-        int currentListItem = getArguments().getInt("position");
+        currentListItem = getArguments().getInt("position");
         playMusic(list.get(currentListItem));
 
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        music_img = (ImageView) getActivity().findViewById(R.id.background);
+        animation = AnimationUtils.loadAnimation(getActivity(), R.anim.img_animtion);
+        LinearInterpolator lin = new LinearInterpolator();//设置动画匀速运动
+        animation.setInterpolator(lin);
+        music_img.startAnimation(animation);
+
+        btn_play = (ImageView) getActivity().findViewById(R.id.play);
+        btn_play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(myMediaPlayer.isPlaying()){
+                    btn_play.setImageResource(R.drawable.ic_play);
+                    myMediaPlayer.pause();
+                    music_img.clearAnimation();
+                }else{
+                    btn_play.setImageResource(R.drawable.pause);
+                    myMediaPlayer.start();
+                    music_img.startAnimation(animation);
+                }
+            }
+        });
+
+        btn_previous = (ImageView) getActivity().findViewById(R.id.previous);
+        btn_previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                previousMusic();
+            }
+        });
+
+        btn_next = (ImageView) getActivity().findViewById(R.id.next);
+        btn_next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                nextMusic();
+            }
+        });
+
+
+
     }
 
     public void playMusic(MusicModel musicModel){
@@ -64,40 +115,16 @@ public class MusicFragment extends Fragment implements View.OnClickListener{
         if(++currentListItem >= list.size()){
             currentListItem = 0;
         }
-        else{
-            playMusic(list.get(currentListItem));
-        }
+        playMusic(list.get(currentListItem));
     }
 
     void previousMusic(){
-        if(currentListItem != 0)
-        {
-            if(--currentListItem >= 0){
-                currentListItem = list.size();
-            } else{
-                playMusic(list.get(currentListItem));
-            }
-        }  else{
-            playMusic(list.get(0));
+        if(--currentListItem < 0) {
+            currentListItem = 0;
         }
+        playMusic(list.get(currentListItem));
     }
 
-    public void onClick(View view){
-        switch (view.getId()){
-            case R.id.next:
-                nextMusic();
 
-            case R.id.previous:
-                previousMusic();
 
-            case R.id.play:
-                if(myMediaPlayer.isPlaying()){
-                    btn_play.setBackgroundResource(R.drawable.pause);
-                    myMediaPlayer.pause();
-                }else{
-                    btn_play.setBackgroundResource(R.drawable.ic_play);
-                    myMediaPlayer.start();
-                }
-        }
-    }
 }
